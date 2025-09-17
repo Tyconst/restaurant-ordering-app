8d94dc0cd29285dd9ef6d66d85ddab46c4c4fca8
Tyconst <224600642+Tyconst@users.noreply.github.com>
2025-09-17 16:32:11 -0400

    Update JS functionality
    Renders HTML via JS, creates cart. Add and remove button functionality

--- index.js
+++ index.js
@@ -1,74 +1,118 @@
 import { menuArray } from '/data.js'
-const cartArr = []
+let cartArray = []
 
-document.addEventListener("click", function(e){
-    if (e.target.dataset.add){
+// Add event listener for click of add buttons, need to make sure they are for respective item
+document.addEventListener('click', function(e){
+    if(e.target.dataset.add){
         handleAddClick(e.target.dataset.add)
     }
-    else if (e.target.id === 'checkout-btn')
-        handleCheckoutBtnClick()
-}) 
+    else if(e.target.dataset.remove)
+        handleRemoveClick(e.target.dataset.remove)
 
-//on click find correct menu item and push to cart array, then render cart array
-function handleAddClick (itemId){
+})
+
+
+// Iterate over menuArray to use ID saved in itemId to identify the added item's object.
+// Save object in new const, targetMenuObj
+// Push to cartArray on click
+function handleAddClick(itemId){
     const targetMenuObj = menuArray.filter(function(item){
-        return item.id === itemId
+        // item.id in HTML is a number, but retrieving it via e.target.dataset.add it comes back as a string.
+        // Need to convert to number using either Number() or parseInt()
+        return item.id === Number(itemId)
     })[0]
-    cartArr.push(targetMenuObj)
-    renderCart ()
-}
-
-// render items on click of add button and display html
-function renderCart() {
-    const cartHtml = cartArr.map(function(item){
-        return `
-        <div class="cart-items">
-            <div class="cart-item">
-                <p class="cart-item-name">${item.name}</p>
-                <button class="remove-btn" data-remove="${item.id}">remove</button>
-                <p class="cart-item-price">$${item.price}</p>
-            </div>
-        </div>
-        `
-    }).join('')
-
-//calculate total price of items in cart
-const totalPrice = cartArr.reduce((total, current) => total + current.price, 0)
-
-
-//add to total html
-const totalHtml = `
-        <div class="checkout-price">
-            <p class="total-price-text">Total Price:</p>
-            <p class="total-price">$${totalPrice}</p>
-        </div>
-`
-const cartContainer = document.getElementById('cart-container')
-cartContainer.innerHtml = cartArr.length > 0 ? cartHtml + totalHtml : ''
-
+    cartArray.push(targetMenuObj)
+    render()
 }
 
 
+// Iterate over cartArray to use ID saved in itemId to identify the removed item's object.
+// Save object in new const, targetCartObj
+// Remove from cartArray
+function handleRemoveClick(itemId){
+    const targetIndex = cartArray.findIndex(function(item){
+        return item.id === Number(itemId)
+    })
+    if(targetIndex != -1){
+        cartArray.splice(targetIndex, 1)
+    }
+    render()
+}
 
-//display menu items
-const menuHtml = menuArray.map(function(item){
-    return`
+
+// Get data from menuArray creating a get html function
+function getMenuHtml(){
+    let menuHtml = ``
+
+    menuArray.forEach(function(item){
+        menuHtml += `
         <div class="item">
-                <p class="item-emoji">${item.emoji}</p>
+            <p class="item-emoji">${item.emoji}</p>
             <div class="item-text-container">
                 <p class="item-name" id="item-name">${item.name}</p>
                 <p class="item-ingredients" id="item-ingredients">${item.ingredients}</p>
                 <p class="item-price" id="item-price">$${item.price}</p>
             </div>
-            <button class="item-add-btn" data-add="${item.id}">+</button>
-        </div>
+                <button class="item-add-btn" data-add="${item.id}">+</button>
+            </div>
+        `
+    })
+    return menuHtml
+}
+
+
+// If array contains items, should be displayed under the menu html in "your order"
+function getCartHtml(){
+    if(cartArray.length === 0){
+        return ''
+    }
+
+    let cartItemsHtml = ''
+    const totalPrice = cartArray.reduce((total, current) => total + current.price, 0)
+
+    cartArray.forEach(function(cartItem){
+        cartItemsHtml += `
+            <div class="cart-item">
+                <p class="cart-item-name">${cartItem.name}</p>
+                <button class="remove-btn" data-remove="${cartItem.id}">remove</button>
+                <p class="cart-item-price">$${cartItem.price}</p>
+            </div>
+        `
+    })
+    return `
+    <div class="cart-items">
+    <p class="your-order"> Your Order</p>
+    ${cartItemsHtml}
+    </div>
+    <div class="checkout-price">
+        <p class="total-price-text">Total Price:</p>
+        <p class="total-price">$${totalPrice}</p>
+    </div>
     `
-}).join('')
-
-document.getElementById('menu').innerHTML = menuHtml
+}
 
 
 
+
+// Create a render function setting elements inner html to the menuHtml
+// call render function
+function render(){
+document.getElementById('menu').innerHTML = getMenuHtml()
+document.getElementById('cart').innerHTML = getCartHtml()
+}
+
+render()
+
+
+
+
+
+// Each item should have a remove button and checkout button at bottom
+
+// When checkout button is clicked, modal should appear with input form
+// Form should have 3 inputs that are compulsory and complete order button
+// Add complete order button to event listener
+// On click, modal should disappear and html containing order should change and display thank you message
 
 
 
