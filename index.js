8b1cfaf93aaaf7d388c13a81dbe6461819c56766
Tyconst <224600642+Tyconst@users.noreply.github.com>
2025-09-19 09:02:31 -0400

    Add checkout functionality
    Renders modal with form, gets users name, displays thank you message on submit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { menuArray } from '/data.js'
 let cartArray = []
 const modalInner = document.getElementById("modal")
+const cartInner = document.getElementById("cart")
+const checkoutMessage = document.getElementById("checkout-message")
+
+
+
 
 // Add event listener for click of add buttons, need to make sure they are for respective item
+// Add complete order button to event listener
 document.addEventListener('click', function(e){
     if(e.target.dataset.add){
         handleAddClick(e.target.dataset.add)
@@ -15,34 +21,6 @@ document.addEventListener('click', function(e){
     }
 })
 
-
-// When checkout button is clicked, modal should appear with input form
-function renderCheckout(){
-    modalInner.innerHTML = `
-                    <form>
-                    <div class="modal-header">
-                    <h3>Enter card details</h3>
-                    </div>
-                    <div class="modal-inputs">
-                    <input type="text" 
-                    placeholder="Enter your name" 
-                    required>
-                    <input 
-                    type="text" 
-                    placeholder="Enter card number" 
-                    required>
-                    <input 
-                    type="text" 
-                    placeholder="Enter CVV" 
-                    required>
-                    </div>
-                    <div class="modal-submit">
-                    <button type="submit" id="pay-btn">Pay</button>
-                    </div>
-                </form>
-    `
-    modalInner.style.display = 'flex'
-}
 
 // Iterate over menuArray to use ID saved in itemId to identify the added item's object.
 // Save object in new const, targetMenuObj
@@ -124,7 +102,65 @@ function getCartHtml(){
             `
 }
 
+// When checkout button is clicked, modal should appear with input form
+// Form should have 3 inputs that are compulsory and complete order button
+function renderCheckout(){
+    modalInner.innerHTML = `
+                    <form id="submit-form">
+                    <div class="modal-header">
+                        <h3>Enter card details</h3>
+                    </div>
+                    <div class="modal-inputs">
+                        <input type="text"
+                        id="customerName"
+                        name="customerName" 
+                        placeholder="Enter your name" 
+                        pattern="[a-zA-Z]+"
+                        required>
+                        <input 
+                        type="text"
+                        id="creditCardNumber"
+                        name="creditCardNumber"  
+                        placeholder="Enter card number"
+                        pattern="[0-9]{16}" 
+                        required>
+                        <input 
+                        type="text"
+                        id="creditCardCVV"
+                        name="creditCardCVV"  
+                        placeholder="Enter CVV"
+                        pattern="[0-9]{3,4}" 
+                        required>
+                    </div>
+                    <div class="modal-submit">
+                        <button type="submit" id="pay-btn">Pay</button>
+                    </div>
+                </form>
+    `
+    modalInner.style.display = 'flex'
 
+    const submitForm = document.getElementById("submit-form")
+    submitForm.addEventListener('submit', function(e){
+        e.preventDefault()
+        renderThankYouMessage()
+    })
+
+}
+
+
+
+function renderThankYouMessage(){
+    const submitForm = document.getElementById("submit-form")
+    const submitFormData = new FormData(submitForm)
+    const name = submitFormData.get('customerName')
+    
+    modalInner.style.display = 'none'
+    cartInner.style.display = 'none'
+    checkoutMessage.style.display ='flex'
+    checkoutMessage.innerHTML = `
+    <p>Thanks, ${name}! Your order is on it's way!</p>
+    `
+}
 
 
 // Create a render function setting elements inner html to the menuHtml
@@ -137,14 +173,6 @@ document.getElementById('cart').innerHTML = getCartHtml()
 render()
 
 
-
-
-
-// Each item should have a remove button and checkout button at bottom
-
-
-// Form should have 3 inputs that are compulsory and complete order button
-// Add complete order button to event listener
 // On click, modal should disappear and html containing order should change and display thank you message
 
 
